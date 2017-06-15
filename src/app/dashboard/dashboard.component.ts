efd2feb4dc20fb3a7d7df499bba91499d1b21edc
paulbert <paul.r.albert@gmail.com>
2017-06-15 11:46:45 -0400

    Add dashboard test spec

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 import { UserService } from '../shared/user.service';
 
+// Main page once logged in.  At this stage is more of a placeholder.
 @Component({
     template: `
-        <div>Hi, {{name}}</div>
+        <div id="greeting">Hi, {{name}}</div>
     `
 })
 export class DashboardComponent implements OnInit {
